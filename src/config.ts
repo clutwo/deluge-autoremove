fc1727e902bdbb76fc28fd6a44b636f06f422cf7
Clemens <clemens.luschnig@gmail.com>
2019-10-29 12:31:43 +0100

    absolute folder for config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 import ini from 'ini';
 import fs from "fs";
-let config = ini.parse(fs.readFileSync('./config/config.ini', 'utf-8'));
+let config = ini.parse(fs.readFileSync('/app/config/config.ini', 'utf-8'));
 export { config };
